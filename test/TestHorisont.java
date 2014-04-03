a96de7d2c4aed7bb7633377376a1954ff201dc60
Hagfjall <hagfjall@gmail.com>
2014-04-03 19:15:36 +0200

    påbörjad höjdkontroll

--- test/TestHorisont.java
+++ test/TestHorisont.java
@@ -2,50 +2,65 @@ package test;
 
 import game.Board;
 import game.blocks.Shape;
-import game.blocks.Square;
+import game.blocks.T;
 
-public class TestHorisont {
+public class TestHorisont extends Thread {
 
 	Board b;
 	Shape s;
 	int x, y;
 
 	public static void main(String[] args) {
-		new TestHorisont().test();
+		new TestHorisont().run();
 
 	}
 
-	private void test() {
+	@Override
+	public void run() {
 		x = 10;
 		y = 22;
 		b = new Board(x, y);
-		s = new Square();
+		s = new T();
 		addValues();
+		s.rotate(true);
+		putShapeInBoard(3,15);
 		printBoard();
+		//pause på 1 sek
+//		printBoard();
 	}
+
 	private void printBoard() {
 		for (int iy = 0; iy < y; iy++) {
 			for (int ix = 0; ix < x; ix++) {
-				if(b.checkSlot(ix, iy))
-					System.out.print(b.getType(ix, iy));
+				if (b.checkSlot(ix, iy)){
+						System.out.print(b.getType(ix, iy));
+				}
 				else
 					System.out.print(".");
 			}
 			System.out.println(" ");
 		}
 	}
+	private void putShapeInBoard(int ix, int iy){
+		for(int i = 0; i < s.getHeight(); i++){
+			for(int j = 0; j < s.getWidth(); j++){
+				if(s.checkSlot(i, j))
+						b.setSlot(ix + i, iy + j, s.getType());
+			}
+		}
+	}
 
 	private void addValues() {
-				b.setSlot(0,21, (byte)1); 
-				b.setSlot(1,21, (byte)1); 
-				b.setSlot(2,19, (byte)1); 
-				b.setSlot(3,19, (byte)1);
-				b.setSlot(4,19, (byte)1);
-				b.setSlot(5,18, (byte)1);
-				b.setSlot(6,19, (byte)1);
-				b.setSlot(7,20, (byte)1);
-				b.setSlot(8,20, (byte)1);
-				b.setSlot(9,21, (byte)1);
+		b.setSlot(0, 21, (byte) 1);
+		b.setSlot(1, 21, (byte) 1);
+		b.setSlot(2, 19, (byte) 1);
+		b.setSlot(3, 19, (byte) 1);
+		b.setSlot(4, 19, (byte) 1);
+		b.setSlot(5, 18, (byte) 1);
+		b.setSlot(6, 19, (byte) 1);
+		b.setSlot(7, 20, (byte) 1);
+		b.setSlot(8, 20, (byte) 1);
+		b.setSlot(9, 21, (byte) 1);
 	}
 
 }
