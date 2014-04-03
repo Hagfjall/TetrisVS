6103130fd0014d2b43504eaf4265f5a7d179aee7
Johan <tojohansvensson@gmail.com>
2014-04-03 13:05:15 +0200

    Johan

--- test/TestBoard.java
+++ test/TestBoard.java
@@ -8,26 +8,25 @@ public class TestBoard {
 		int x = 10, y = 10;
 		Board b = new Board(x, y);
 		b.setSlot(0, 0, (byte) 10);
-		System.out.println("should be false: "
-				+ Boolean.toString(b.checkSlot(0, 0)));
-		System.out.println("should be true: "
-				+ Boolean.toString(b.checkSlot(1, 1)));
 		
 		for (int i = 0; i < x; i++) {
 			for (int j = 0; j < y; j++) {
-				if (j == 0 || j == y - 1) {
+				if (j == 0 ) {
 					System.out.print("|");
 				}
 				if (b.checkSlot(i, j))
-					System.out.print(" ");
-				else
 					System.out.print("#");
+				else
+					System.out.print(" ");
+				if( j == y - 1){
+					System.out.print("|");
+				}
 			}
 			System.out.println(" ");
 		}
 		System.out.print(" ");
-		for (int i = 0; i < x - 1; i++)
-			System.out.print("¨");
+		for (int i = 0; i < x; i++)
+			System.out.print("-");
 	}
 
 }
