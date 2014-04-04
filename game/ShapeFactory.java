2e1d1d252bc8df8448f49fd9c1cc29e378a9cd19
Hagfjall <hagfjall@gmail.com>
2014-04-04 11:24:16 +0200

    ShapeFactory skapad

---
+++ game/ShapeFactory.java
@@ -0,0 +1,50 @@
+package game;
+
+import game.blocks.*;
+
+import java.util.Random;
+
+public class ShapeFactory {
+
+	private Random rand;
+
+	public ShapeFactory(long randomseed) {
+		rand = new Random(randomseed);
+	}
+
+	/**
+	 * return a shape randomly
+	 * @return Shape
+	 */
+	public Shape getShape() {
+		byte rnd = (byte) rand.nextInt(Shape.I + 1);
+		return getShape(rnd);
+	}
+
+	/**
+	 * returns the specified shape representing the type
+	 * @param type one of the Shape-constant
+	 * @return
+	 */
+	public Shape getShape(byte type) {
+		switch (type) {
+		case Shape.I:
+			return new I();
+		case Shape.L_LEFT:
+			return new L_Left();
+		case Shape.L_RIGHT:
+			return new L_Right();
+		case Shape.SQUARE:
+			return new Square();
+		case Shape.T:
+			return new T();
+		case Shape.Z_LEFT:
+			return new Z_Left();
+		case Shape.Z_RIGHT:
+			return new Z_Right();
+		default:
+			return null;
+		}
+	}
+
+}
