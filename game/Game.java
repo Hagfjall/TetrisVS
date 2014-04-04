382f59c5364eea4823e808f69102db3d6b3cd3fc
paffare <anton.lundborg@gmail.com>
2014-04-04 12:12:25 +0200

    Added more ame logic

--- game/Game.java
+++ game/Game.java
@@ -3,7 +3,6 @@ package game;
 import game.blocks.Shape;
 import game.blocks.ShapeFactory;
 
-
 public class Game extends Thread {
 	private Board board;
 	private Shape currentShape;
@@ -18,41 +17,52 @@ public class Game extends Thread {
 	}
 
 	private boolean checkLeft() {
+		return false;
 
 	}
 
 	private boolean checkRight() {
-
+		return false;
 	}
 
 	private boolean checkOneDown() {
 		boolean free = true;
-		
+		return false;
+
 	}
 
-	private void getShape() {
-		currentShape = factory.getShape();
-	}
-
-	//Tries to set a new shape. If false, game is lost.
+	/**
+	 * Tries to set a new shape. If false, game is lost.
+	 * 
+	 * @return false if shapes exists on the spawn-spot
+	 */
 	private boolean newShape() {
-		getShape();
-		currentX = (int) Math.round(((double)board.getWidth() / 2) - ((double)currentShape.getWidth() / 2));
+		currentShape = factory.getShape();
+		currentX = (int) Math.round(((double) board.getWidth() / 2)
+				- ((double) currentShape.getWidth() / 2));
 		currentY = 0;
+		for (int r = 0; r < currentShape.getHeight(); r++) {
+			for (int c = 0; c < currentShape.getWidth(); c++) {
+				if(board.checkSlot(currentX + r,currentY + c) && currentShape.checkSlot(r, c)){
+					return false;
+				}
+			}
+		}
 		putShapeOnBoard();
 		return true;
 	}
-	
+
 	private void putShapeOnBoard() {
 		for (int r = 0; r < currentShape.getHeight(); r++) {
-			for(int c = 0; c < currentShape.getWidth(); c++ ){
-				if(currentShape.checkSlot(r, c)) {
-					board.setSlot(currentX + r, currentY +c, currentShape.getType());
+			for (int c = 0; c < currentShape.getWidth(); c++) {
+				if (currentShape.checkSlot(r, c)) {
+					board.setSlot(currentX + r, currentY + c,
+							currentShape.getType());
 				}
 			}
 		}
 	}
-	
+
 	private void failGame() {
 
 	}
