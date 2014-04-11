2a7b0cc01a9a7ba64beed3a8b38c35a8aa24f932
paffare <anton.lundborg@gmail.com>
2014-04-11 11:47:40 +0200

    ingen aning / fredrik

--- game/GameBoard.java
+++ game/GameBoard.java
@@ -6,29 +6,45 @@ import game.blocks.Shape;
 
 public class GameBoard extends Board {
 
-	
 	public GameBoard(int width, int height) {
 		super(width, height);
 	}
-	
-	public void setShape(Point p, Shape s){
-		for(int row = 0; row < s.getWidth(); row++){
-			for (int col = 0; col < s.getHeight(); col++){
-				if(s.checkSlot(row, col)){
-					setSlot(row, col, s.getType());
+
+	public void setShape(Point p, Shape s) {
+		int[] rowsAffected = new int[s.getWidth()];
+		for (int row = 0; row < s.getWidth(); row++) {
+			for (int col = 0; col < s.getHeight(); col++) {
+				if (s.checkSlot(row, col)) {
+					setSlot(p.y + row, p.x + col, s.getType());
+					rowsAffected[row] = p.y + row;
 				}
 			}
 		}
-	}
-	
-
-	public void removeRows(int nbrrows) {
-		byte[][] temp = new byte[width][height];
-		for (int r = 0; r < height-nbrrows; r++) {
-			for (int c = 0; c < width; c++) {
-				temp[c][r+nbrrows] = board[c][r];
+		for (int i = 0; i < rowsAffected.length; i++) {
+			if (isRowFull(rowsAffected[i])) {
+				removeRow(rowsAffected[i]);
 			}
 		}
-		board = temp;
+	}
+
+	private boolean isRowFull(int row) {
+		for (int col = 0; col < width; col++) {
+			if (!checkSlot(row, col)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void removeRow(int startRow) {
+		byte[][] temp = new byte[width][height];
+		for(int row = startRow; row > 0; row--){
+			for(int col = 0; col < getWidth(); col++){
+				if(checkSlot(row - 1, col)){
+					setSlot(row, col, type)
+				}
+				this.setSlot(row, col, type)
+		}
+		}
 	}
 }
