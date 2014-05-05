10fbc75c12c8f80086b94f7445d507f005df2512
Johan <tojohansvensson@gmail.com>
2014-05-05 14:11:11 +0200

    Powerups meckat

--- game/powerups/Powerup.java
+++ game/powerups/Powerup.java
@@ -1,18 +1,31 @@
 package game.powerups;
 
+import javax.swing.Timer;
+
 public abstract class Powerup {
 
+	public static final byte NULLPOWERUP = 0;
 	public static final byte INCSPEED = 1;
 	public static final byte MIRROR = INCSPEED + 1;
+	public static final byte SINGLEBLOCK = INCSPEED + 1;
 
 	protected byte type;
+	protected long startTime;
 
 	public Powerup(byte type) {
 		this.type = type;
+		startTime = System.currentTimeMillis() + 30000;
+	}
+
+	public Powerup() {
 	}
 
 	public byte getType() {
 		return type;
+	}
+
+	public boolean isActive() {
+		return System.currentTimeMillis() < startTime;
 	}
 
 }
---
+++ game/powerups/SingleBlock.java
@@ -0,0 +1,9 @@
+package game.powerups;
+
+public class SingleBlock extends Powerup {
+
+	public SingleBlock(byte type) {
+		super(Powerup.SINGLEBLOCK);
+	}
+
+}
