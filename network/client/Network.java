cd0bc43b557a7af1894c1d11e00064fdd6a67893
Fredrik Hagfjäll <hagfjall@gmail.com>
2014-05-07 11:44:23 +0200

    Network sammanslaget

---
+++ network/client/Network.java
@@ -0,0 +1,104 @@
+package network.client;
+
+import game.Game;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import network.ProtocolConstants;
+
+public class Network implements Runnable {
+
+	private final Game localGame, opponentGame;
+	private final Socket socket;
+	private DataOutputStream out;
+	private DataInputStream in;
+
+	public Network(Socket socket, Game localGame, Game opponentGame) {
+		this.socket = socket;
+		this.localGame = localGame;
+		this.opponentGame = opponentGame;
+		try {
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sendKey(byte key) {
+		send(key);
+	}
+
+	public void sendPowerupAck(byte pwrUp) {
+		send(ProtocolConstants.POWERUP_ACK);
+		send(pwrUp);
+	}
+
+	@Override
+	public void run() {
+		DataInputStream in;
+		try {
+			in = new DataInputStream(this.socket.getInputStream());
+			int read;
+			while ((read = in.read()) != -1) {
+				switch (read) {
+				case ProtocolConstants.STRING:
+					System.err.println("read String ska inte göras");
+					break;
+				case ProtocolConstants.INT:
+					System.err.println("Read Int ska inte göras");
+					break;
+				case ProtocolConstants.LEFT:
+					localGame.moveLeft();
+					break;
+				case ProtocolConstants.UP:
+					opponentGame.rotateClockwise();
+					break;
+				case ProtocolConstants.RIGHT:
+					opponentGame.moveRight();
+					break;
+				case ProtocolConstants.DOWN:
+					opponentGame.rotateCounterClockwise();
+					break;
+				case ProtocolConstants.SPACE:
+					opponentGame.moveBottom();
+					break;
+				case ProtocolConstants.X:
+					System.out.print("recieving powerup");
+					byte pwrUp = in.readByte();
+					System.out.println(" nbr " + pwrUp);
+					localGame.usePowerup(pwrUp);
+					sendPowerupAck(pwrUp);
+					break;
+				case ProtocolConstants.POWERUP_ACK:
+					System.out.print("recieving powerupACK");
+					pwrUp = in.readByte();
+					System.out.println(" nbr " + pwrUp);
+					opponentGame.usePowerup(pwrUp);
+					break;
+				case ProtocolConstants.MOVEDOWN:
+					opponentGame.moveDown();
+					break;
+				} // TODO implementera resten
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void send(byte type) {
+		try {
+			out.write(type);
+		} catch (IOException e) {
+			e.printStackTrace();
+			try {
+				socket.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+}
