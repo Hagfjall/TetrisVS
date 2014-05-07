1c21d6c45b9878f0ae0f53c30f261e2411306d0c
paffare <anton.lundborg@gmail.com>
2014-05-07 14:45:33 +0200

    Added som padding in the statspanel in gui

--- gui/StatsPanel.java
+++ gui/StatsPanel.java
@@ -5,6 +5,7 @@ import game.Game;
 import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class StatsPanel extends JPanel {
 
@@ -13,12 +14,14 @@ public class StatsPanel extends JPanel {
 
 	public StatsPanel(String localName, String opponentName, Game localGame,
 			Game opponentGame) {
-		setLayout(new BorderLayout());
+		setLayout(new BorderLayout(50, 0));
 		localPanel = new StatPanel(localName, localGame);
+		localPanel.setBorder(new EmptyBorder(47, 10, 10, 10));
 		opponentPanel = new StatPanel(opponentName, opponentGame);
+		opponentPanel.setBorder(new EmptyBorder(10, 10, 20, 10));
 		add(localPanel, BorderLayout.NORTH);
+
 		add(opponentPanel, BorderLayout.SOUTH);
 
 	}
-
 }
