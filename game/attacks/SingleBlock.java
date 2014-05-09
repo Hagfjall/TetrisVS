b6e83eeafa2bc2a6262eb3a578677df2223b23b5
Fredrik Hagfjäll <hagfjall@gmail.com>
2014-05-09 15:42:57 +0200

    kommenterar för full

--- game/attacks/SingleBlock.java
+++
@@ -1,9 +0,0 @@
-package game.attacks;
-
-public class SingleBlock extends Attack {
-
-	public SingleBlock() {
-		super(Attack.SINGLEBLOCK);
-	}
-
-}
--- gui/menu/Menu.java
+++
@@ -1,7 +0,0 @@
-package gui.menu;
-
-import javax.swing.JMenu;
-
-public class Menu extends JMenu {
-
-}
